6b9f48bbc9d24bfe655fe45c76a685e76b588fa8
[Blaise Larrabee] <blaiselarrabee@gmail.com>
2020-10-04 23:41:26 -0600

    added email to local config of git

--- clock/script.js
+++ clock/script.js
@@ -1,9 +1,9 @@
+
+
+
 const HOURHAND = document.querySelector("#hour");
 const MINUTEHAND = document.querySelector("#minute");
 const SECONDHAND = document.querySelector("#second");
-
-
-
 
 
 // create date object with curent date and time
